ab443c08c149518c44996d80057f1d39f4b3ed04
danilNil <danil.nikiphorov@gmail.com>
2014-04-18 23:21:15 +0700

    added tests for argument

--- KiwiUnitTest/KiwiUnitTest.m
+++ KiwiUnitTest/KiwiUnitTest.m
@@ -1,6 +1,5 @@
 #import "Kiwi.h"
 #import "LoginViewController.h"
-#import "CoreComponentsFactory.h"
 #import "Typhoon.h"
 
 @interface LoginViewController ()
@@ -17,12 +16,36 @@ describe(@"initialisation all services", ^{
     context(nil, ^{
         describe(@"Login View Controller", ^{
             LoginViewController* loginVC = [LoginViewController new];
-            context(@"Did input non empty password and mail", ^{
-                //            loginVC.loginTextField.text = @"test";
-                //            loginVC.passwordTextField.text = @"test";
+            context(@"On press Login", ^{
                 it(@"should call Api service", ^{
                     loginVC.apiService = [KWMock mockForProtocol:@protocol(ApiService)];
                     [[(NSObject*)loginVC.apiService should] receive:@selector(loginWithUsername:password:completion:)];
+                    [loginVC onLogin:nil];
+                });
+            });
+            
+            context(@"On press Login with existed login and pass", ^{
+                it(@"should call Api service with test pass and test login", ^{
+                NSString* pass = @"testPass";
+                NSString* login = @"testLogin";
+                    loginVC.loginTextField = [KWMock mockForClass:[UITextField class]];
+                    loginVC.passwordTextField = [KWMock mockForClass:[UITextField class]];
+                    [loginVC.passwordTextField stub:@selector(text) andReturn:pass];
+                    [loginVC.loginTextField stub:@selector(text) andReturn:login];
+                    loginVC.apiService = [KWMock mockForProtocol:@protocol(ApiService)];
+                    [[(NSObject*)loginVC.apiService should] receive:@selector(loginWithUsername:password:completion:) withArguments:login, pass, any()];
+                    [loginVC onLogin:nil];
+                });
+            });
+            
+            context(@"On press Login with empty login and pass", ^{
+                it(@"shouldn't call Api service with test pass and test login", ^{
+                    loginVC.loginTextField = [KWMock mockForClass:[UITextField class]];
+                    loginVC.passwordTextField = [KWMock mockForClass:[UITextField class]];
+                    [loginVC.passwordTextField stub:@selector(text) andReturn:@""];
+                    [loginVC.loginTextField stub:@selector(text) andReturn:@""];
+                    loginVC.apiService = [KWMock mockForProtocol:@protocol(ApiService)];
+                    [[(NSObject*)loginVC.apiService shouldNot] receive:@selector(loginWithUsername:password:completion:) withArguments:@"", @"", any()];
                     [loginVC onLogin:nil];
                 });
             });
