be2e7b939f02c490f06e93832ad78abf3287f544
Brian Gesiak <modocache@gmail.com>
2014-04-19 06:11:35 +0900

    Some suggestions for more behavioral tests.

--- KiwiUnitTest/KiwiUnitTest.m
+++ KiwiUnitTest/KiwiUnitTest.m
@@ -2,55 +2,52 @@
 #import "LoginViewController.h"
 #import "Typhoon.h"
 
-@interface LoginViewController ()
-//Testing internal methods is inherently brittle because internal implementation details are by nature the part of a class that changes the most. Your testing experience will be much more fun when it goes through a more stable public interface for the majority of classes.	https://github.com/allending/Kiwi/wiki/Kiwi-FAQ#q-how-do-i-test-my-privateinternal-methods
-//Actually you should create some public interface for all of your classes it make it more testable and more supported.
-@property (weak, nonatomic) IBOutlet UITextField *loginTextField;
-@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
-- (IBAction)onLogin:(id)sender;
-
-@end
-
 SPEC_BEGIN(LoginViewControllerSpec)
-describe(@"initialisation all services", ^{
-    
-    context(nil, ^{
-        describe(@"Login View Controller", ^{
-            LoginViewController* loginVC = [LoginViewController new];
-            context(@"On press Login", ^{
-                it(@"should call Api service", ^{
-                    loginVC.apiService = [KWMock mockForProtocol:@protocol(ApiService)];
-                    [[(NSObject*)loginVC.apiService should] receive:@selector(loginWithUsername:password:completion:)];
-                    [loginVC onLogin:nil];
-                });
+
+/// Remove outer describe and context blocks, since they seemed unnecessary.
+describe(@"LoginViewController", ^{
+    /// By initializing the login view controller in a beforeEach block, we
+    /// ensure that it is a "fresh" instance each test--i.e.: it does not
+    /// retain any state from previous tests.
+    __block LoginViewController* loginVC = nil;
+    beforeEach(^{
+        loginVC = [LoginViewController new];
+    });
+
+    describe(@"-onLogin:", ^{
+        /// Because we are testing the view controller's buttons, we must
+        /// ensure that -loadView is called before each test, so here we
+        /// trigger -loadView by accessing the -view. We also set up the
+        /// API mock, since we'll be using it in each of these tests.
+        beforeEach(^{
+            loginVC.apiService = [KWMock mockForProtocol:@protocol(ApiService)];
+            [loginVC view];
+        });
+
+        /// This is identical to the third test in the original code:
+        /// "On press Login with empty login and pass". So I deleted the last test.
+        it(@"calls the API service", ^{
+            [[(NSObject*)loginVC.apiService should] receive:@selector(loginWithUsername:password:completion:)];
+            [loginVC.loginButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+        });
+
+        context(@"when the login and password fields are filled in", ^{
+            NSString *login = @"testLogin";
+            NSString *password = @"testPassword";
+
+            /// No reason to mock these. We can set their values for realsies.
+            beforeEach(^{
+                loginVC.loginTextField.text = login;
+                loginVC.passwordTextField.text = password;
             });
-            
-            context(@"On press Login with existed login and pass", ^{
-                it(@"should call Api service with test pass and test login", ^{
-                NSString* pass = @"testPass";
-                NSString* login = @"testLogin";
-                    loginVC.loginTextField = [KWMock mockForClass:[UITextField class]];
-                    loginVC.passwordTextField = [KWMock mockForClass:[UITextField class]];
-                    [loginVC.passwordTextField stub:@selector(text) andReturn:pass];
-                    [loginVC.loginTextField stub:@selector(text) andReturn:login];
-                    loginVC.apiService = [KWMock mockForProtocol:@protocol(ApiService)];
-                    [[(NSObject*)loginVC.apiService should] receive:@selector(loginWithUsername:password:completion:) withArguments:login, pass, any()];
-                    [loginVC onLogin:nil];
-                });
-            });
-            
-            context(@"On press Login with empty login and pass", ^{
-                it(@"shouldn't call Api service with test pass and test login", ^{
-                    loginVC.loginTextField = [KWMock mockForClass:[UITextField class]];
-                    loginVC.passwordTextField = [KWMock mockForClass:[UITextField class]];
-                    [loginVC.passwordTextField stub:@selector(text) andReturn:@""];
-                    [loginVC.loginTextField stub:@selector(text) andReturn:@""];
-                    loginVC.apiService = [KWMock mockForProtocol:@protocol(ApiService)];
-                    [[(NSObject*)loginVC.apiService shouldNot] receive:@selector(loginWithUsername:password:completion:) withArguments:@"", @"", any()];
-                    [loginVC onLogin:nil];
-                });
+
+            it(@"calls the API service with those values", ^{
+                [[(NSObject *)loginVC.apiService should] receive:@selector(loginWithUsername:password:completion:)
+                                                   withArguments:login, password, [KWAny any], nil];
+                [loginVC.loginButton sendActionsForControlEvents:UIControlEventTouchUpInside];
             });
         });
     });
 });
+
 SPEC_END
